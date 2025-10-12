b51e623415f76d71fcd0d23b525427747881ac98
Daniel Kirstenpfad <bietiekay@schrankmonster.de>
2025-10-12 14:51:41 +0200

    - empty sample app to be filled.

---
+++ Sample/NavigationOverlayKitSample/NavigationOverlayKitSample/NavigationOverlayKitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationOverlayKitSampleApp.swift
+//  NavigationOverlayKitSample
+//
+//  Created by Daniel Kirstenpfad on 12.10.25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationOverlayKitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sample/NavigationOverlayKitSample/NavigationOverlayKitSampleTests/NavigationOverlayKitSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationOverlayKitSampleTests.swift
+//  NavigationOverlayKitSampleTests
+//
+//  Created by Daniel Kirstenpfad on 12.10.25.
+//
+
+import Testing
+@testable import NavigationOverlayKitSample
+
+struct NavigationOverlayKitSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
